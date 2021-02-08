a8b3858be0c5156d610eb8571eeea803d3c9b7ed
VartikaKaul <78537604+VartikaKaul@users.noreply.github.com>
2021-02-08 20:14:34 +0530

    Update main.py

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -19,7 +19,7 @@ import speech_recognition as sr
 
 r=sr.Recognizer()
 print("Please Speak")
-with sr.Microphone as source;
+with sr.Microphone as source:
     audio_data=r.record(source, duration=5)
     print("Recognizing.....")
     text=r.recognize_google(audio_data)
